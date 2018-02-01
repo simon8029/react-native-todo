b855bc5b3b4562c04c558cdc13be039504f9ce25
simon X.L <simon.lau@msn.com>
2018-01-31 21:33:14 -0500

    json-server get and post

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -12,12 +12,31 @@ class Todo extends React.Component {
     this.onCreatePressed = this.onCreatePressed.bind(this);
   }
 
+  componentWillMount() {
+    fetch('http://10.0.1.15:8139/todos', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(todos => this.setState({ todos }))
+  }
+
   onTextInputChange(text) {
     this.setState({ newTodo: text });
   }
 
   onCreatePressed() {
-    this.setState({ todos: [...this.state.todos, this.state.newTodo], newTodo: '' });
+    // this.setState({ todos: [...this.state.todos, this.state.newTodo], newTodo: '' });
+    fetch('http://localhost:8139/todos', {
+      method: 'post',
+      body: JSON.stringify({
+        name: this.state.newTodo
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
 
   render() {
@@ -42,8 +61,8 @@ class Todo extends React.Component {
           </TouchableOpacity>
         </View>
         <View style={styles.View}>
-          {this.state.todos.map((todo, i) =>
-            (<Text key={i} style={styles.Text}>{todo}</Text>)
+          {this.state.todos.map((todo) =>
+            (<Text key={todo.id} style={styles.Text}>{todo.name}</Text>)
           )}
         </View>
       </View>
@@ -52,18 +71,18 @@ class Todo extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20
-  },
-  form: {
-    flexDirection: 'row'
-  },
-  textInput: {
-    flex: 0.7,
-    width: 100,
-    height: 80
-  },
+  // container: {
+  //   flex: 1,
+  //   padding: 20
+  // },
+  // form: {
+  //   flexDirection: 'row'
+  // },
+  // textInput: {
+  //   flex: 0.7,
+  //   width: 100,
+  //   height: 80
+  // },
   button: {
     flex: 0.3,
     borderWidth: 1,
@@ -72,9 +91,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  buttonText: {
-    fontSize: 18
-  }
+  // buttonText: {
+  //   fontSize: 18
+  // }
 });
 
 export default Todo;
